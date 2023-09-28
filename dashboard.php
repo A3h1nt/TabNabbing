c5ba89dcf1761162ae44b52a74f8b9e8571a8336
A3h1nt <56585189+A3h1nt@users.noreply.github.com>
2023-09-28 22:54:48 +0530

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,12 @@
+<title> Safe Website !</title>
+
+<?php
+
+$email=$_POST['email'];
+$password=$_POST['password'];
+
+?>
+<h1>Welcome to the safest website on the internet !</h1>
+<pre>Welcome user : <?php echo $email; ?></pre>
+<pre>Your password is : <?php echo $password; ?></pre>
+<p>Check out this cool link  : <a href="http://127.0.0.1:8080/tabnabbing/tabNabbing.html"  target="_blank" rel="opener">Click Here</a>
